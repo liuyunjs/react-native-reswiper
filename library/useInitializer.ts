4ae558e09f5a8d9b032bcb05870c134d96c4d197
liuyunjs <429695034@qq.com>
2021-08-08 21:41:32 +0800

    删除多余内容

--- library/useInitializer.ts
+++ library/useInitializer.ts
@@ -17,42 +17,9 @@ export const useInitializer = (initialIndex: number) => {
   return useWillMount(() => {
     const values = createValues(initialIndex);
 
-    // const animateTo = (
-    //   toIndex: Animated.Adaptable<number>,
-    //   animated?: boolean,
-    // ) => {
-    //   if (animated !== false) {
-    //     return block([set(values.nextIndex, toIndex), anim.start(), toIndex]);
-    //   }
-    //   return block([
-    //     set(values.index, toIndex),
-    //     set(values.nextIndex, toIndex),
-    //     set(values.position, toIndex),
-    //   ]);
-    // };
-
     return {
       values,
-      // animateTo,
-      // stop: () => anim.stop(),
-      // start: () => anim.start(),
       index: initialIndex,
     };
   });
-
-  // const { values } = ctx;
-  //
-  // const anim = useSpring({
-  //   toValue: values.nextIndex,
-  //   velocity: values.velocity,
-  //   position: values.position,
-  //   onEnd: useConst((state) => {
-  //     return cond(
-  //       eq(state.position, values.nextIndex),
-  //       set(values.index, values.nextIndex),
-  //     );
-  //   }),
-  // });
-  //
-  // return ctx;
 };
